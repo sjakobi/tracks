3c23f2b32233bdb187ce56b35b66e0e1d0405e6c
Simon Jakobi <simon.jakobi@gmail.com>
2017-01-18 19:29:23 +0100

    Everything compiles!

--- TracksClient.java
+++ TracksClient.java
@@ -1,19 +1,20 @@
+import java.util.concurrent.ThreadLocalRandom;
 import java.io.*;
 import java.net.*;
 import java.nio.charset.*;
 import java.nio.file.*;
 import java.util.*;
 
-public class P2PClient {
+public class TracksClient {
   public static final int[] remotePorts = new int[] {
       50001, 50002, 50003, 50004, 50005, 50006, 50007, 50008, 50009, 50010};
-  public static final int timeout = 3; // ms
+  public static final int minTimeout = 200;  // ms
+  public static final int maxTimeout = 1500; // ms
 
-  public final String dateiname;
   public final int port;
-  public final Set<MusikStueck> stuecke;
+  public final TrackStore store;
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     String dateiname = null;
     int port = 0;
 
@@ -21,11 +22,18 @@ public class P2PClient {
       dateiname = args[0];
       port = Integer.parseInt(args[1]);
     } else {
-      System.err.println("Verwendung: java P2PClient <dateiname> <port>");
+      System.err.println("Verwendung: java TracksClient <dateiname> <port>");
       System.exit(1);
     }
+    TrackStore store = new TrackStore();
+    List<String> lines =
+        Files.readAllLines(FileSystems.getDefault().getPath(".", dateiname),
+                           Charset.defaultCharset());
+    for (String s : lines) {
+      store.add(Track.fromString(s));
+    }
     try {
-      P2PClient client = new P2PClient(dateiname, port);
+      TracksClient client = new TracksClient(port, store);
       client.run();
     } catch (Exception e) {
       e.printStackTrace();
@@ -33,54 +41,52 @@ public class P2PClient {
     }
   }
 
-  public P2PClient(String dateiname, int port) throws IOException {
-    Set<MusikStueck> stuecke = new HashSet<>();
-    List<String> zeilen =
-        Files.readAllLines(FileSystems.getDefault().getPath(".", dateiname),
-                           Charset.defaultCharset());
-    for (String s : zeilen) {
-      stuecke.add(MusikStueck.fromString(s));
-    }
-    this.stuecke = stuecke;
-    this.dateiname = dateiname;
+  public TracksClient(int port, TrackStore store) throws IOException {
+    this.store = store;
     this.port = port;
+  }
+
+  public void setRandomTimeout(DatagramSocket socket) throws SocketException {
+    int randomTimeout =
+        ThreadLocalRandom.current().nextInt(minTimeout, maxTimeout + 1);
+    socket.setSoTimeout(randomTimeout);
   }
 
   public void run() throws IOException, SocketException {
     System.out.println("Ich habe folgende Stücke:");
-    for (MusikStueck stueck : stuecke) {
-      System.out.println("    " + stueck);
+    for (Track track : store.tracks) {
+      System.out.println("    " + track);
     }
     DatagramSocket socket = new DatagramSocket(port);
-    socket.setSoTimeout(timeout);
+    setRandomTimeout(socket);
     while (true) {
-      for (int remotePort : remotePorts) {
-        if (remotePort != port) {
-          // Send IHAVE
-          String ihave = ihave();
-          byte[] ihaveBytes = ihave.getBytes();
-          DatagramPacket outPacket = new DatagramPacket(
-              ihaveBytes, ihaveBytes.length, new InetSocketAddress(remotePort));
-          socket.send(outPacket);
+      /*
+for (int remotePort : remotePorts) {
+  if (remotePort != port) {
+    // Send IHAVE
+    String ihave = ihave();
+    byte[] ihaveBytes = ihave.getBytes();
+    DatagramPacket outPacket = new DatagramPacket(
+        ihaveBytes, ihaveBytes.length, new InetSocketAddress(remotePort));
+    socket.send(outPacket);
 
-          // Accept Answers
-          byte[] buffer = new byte[2048];
-          DatagramPacket inPacket = new DatagramPacket(buffer, buffer.length);
-          try {
-            socket.receive(inPacket);
-          } catch (SocketTimeoutException ste) {
-            continue;
-          }
-          String inMsg = inPacket.getData();
-          if (inMsg.startsWith("IHAVE")) {
-            if (inMsg.equals(ihave)) {
-              continue;
-            } else {
-            }
-          }
-        }
-      }
-      public String ihave() {
-        return "IHAVE" + stuecke.size() + "|" + stuecke.hashCode();
+    // Accept Answers
+    byte[] buffer = new byte[2048];
+    DatagramPacket inPacket = new DatagramPacket(buffer, buffer.length);
+    try {
+      socket.receive(inPacket);
+    } catch (SocketTimeoutException ste) {
+      continue;
+    }
+    String inMsg = inPacket.getData();
+    if (inMsg.startsWith("IHAVE")) {
+      if (inMsg.equals(ihave)) {
+        continue;
       }
     }
+  }
+}
+  */
+    }
+  }
+}
